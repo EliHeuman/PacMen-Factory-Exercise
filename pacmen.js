9d185040ae569f2f909760829a50a250aaa39155
EliHeuman <biz.elih@gmail.com>
2021-03-17 22:00:05 +0200

    First commit

---
+++ pacmen.js
@@ -0,0 +1,103 @@
+const pacArray = [
+  ['./images/PacMan1.png', './images/PacMan2.png'],
+  ['./images/PacMan3.png', './images/PacMan4.png'],
+];
+let direction = 0;
+let imgCounter = 0;
+// This array holds all the pacmen
+const pacMen = []; 
+
+// This function returns an object with random values
+function setToRandom(scale) {
+  return {
+    x: Math.random() * scale,
+    y: Math.random() * scale,
+  };
+}
+
+// Factory to make a PacMan at a random position with random velocity
+function makePac() {
+  // returns an object with random values scaled {x: 33, y: 21}
+  const velocity = setToRandom(10); // {x:?, y:?}
+  const position = setToRandom(200);
+  // Add image to div id = game
+  let game = document.getElementById('game');
+  let newimg = document.createElement('img');
+  newimg.style.position = 'absolute';
+  newimg.src = './images/PacMan1.png';
+  newimg.id = imgCounter;
+  newimg.width = Math.random() * 200;
+
+
+  // Sets position here
+  newimg.style.left = position.x;
+  newimg.style.top = position.y + 40; 
+  let pos = 0;
+  let focus = 0;
+  let reverse = true;
+  // Add new Child image to game
+  game.appendChild(newimg);
+
+  // Return details in an object
+  return {
+    position,
+    velocity,
+    newimg,
+    pos,
+    reverse,
+    focus,
+  };
+}
+function update() {
+  // Loop over pacmen array and move each one and move image in DOM
+  pacMen.forEach((item) => {
+    checkCollisions(item);
+    switchImg(item);
+    item.position.x += item.velocity.x;
+    item.position.y += item.velocity.y;
+
+    item.newimg.style.left = item.position.x;
+    item.newimg.style.top = item.position.y;
+  });
+  let timer = setTimeout(update, 60);
+  if(imgCounter == 0){
+    clearTimeout(timer);
+  };
+}
+
+function switchImg(item){
+  item.focus = (item.focus + 1) % 2;
+  if(item.position.x - item.velocity.x + item.newimg.width > window.innerWidth && item.pos ==0 ||
+    item.position.x - item.velocity.x < 0 && item.pos ==1){
+      item.reverse = !item.reverse;
+    }
+    if(item.reverse){
+      item.pos = 0;
+    }else{
+      item.pos = 1;
+    }
+    return item.newimg.src = pacArray[item.pos][item.focus];
+ // setTimeout(switchImg, 200);
+} 
+
+function checkCollisions(item) {
+  // TODO: detect collision with all walls and make pacman bounce
+  if(item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
+    item.position.x + item.velocity.x < 0) item.velocity.x =- item.velocity.x;
+  if(item.position.y + item.velocity.y + item.newimg.height > window.innerHeight ||
+    item.position.y + item.velocity.y < 0) item.velocity.y =- item.velocity.y;
+}
+
+function makeOne() {
+  pacMen.push(makePac()); // add a new PacMan
+  imgCounter = imgCounter + 1;
+}
+
+function reset() {
+  for(let i= 0; i < imgCounter; i++){
+    let elem = document.getElementById(i);
+    elem.remove()
+  }
+  imgCounter = 0;
+ 
+};
